de594ee5ea3a430ca5fe274e159983dfe9f0163c
ragliboltbc <rami.ezzeddine@tum-blockchain.com>
2026-01-12 16:45:34 +0100

    enable escrow contract and test

--- src/Generator/templates/ProcessExecution.sol
+++ src/Generator/templates/ProcessExecution.sol
@@ -1,7 +1,30 @@
 //SPDX-License-Identifier: MIT
+
+import "./EscrowSettlement.sol";
+
 pragma solidity ^0.8.9;
 
 contract {{{modelID}}} {
+  event EscrowCreated(address indexed escrow);
+
+  EscrowSettlement public escrow;
+
+  function createEscrow(address payee) public payable {
+    require(msg.value > 0, "amount required");
+    escrow = new EscrowSettlement{ value: msg.value }(msg.sender, payee, msg.value);
+    emit EscrowCreated(address(escrow));
+  }
+
+  // Rendering of Payment task
+  {{#paymentTasks}}
+  if (id == {{{taskId}}}) {
+    // payer must call enact (or enforce)
+    require(msg.sender == participants[{{{payerIndex}}}], "only payer");
+    // amount must be in wei; use encoding to provide numeric literal or variable
+    createEscrow{ value: {{{amount}}} }(participants[{{{payeeIndex}}}]);
+  }
+  {{/paymentTasks}}
+
   {{^hasSubProcesses}}
   uint public tokenState = 1;
   {{/hasSubProcesses}}

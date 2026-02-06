0e74e9f45ef60a823d0c7e9b21ff38421855e12d
ragliboltbc <rami.ezzeddine@tum-blockchain.com>
2026-02-06 20:54:00 +0100

    fix escrow I

--- src/Generator/templates/EscrowSettlement.sol
+++ src/Generator/templates/EscrowSettlement.sol
@@ -1,18 +1,38 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
-/// EscrowSettlement generated for process
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
 contract EscrowSettlement {
+    enum TokenType { NATIVE, ERC20, ERC721 }
+    
+    TokenType public tokenType;
+    address public tokenAddress;
     address public payer;
     address public payee;
     uint256 public amount;
+    uint256 public tokenId; // for ERC721
     bool public released;
 
-    constructor(address _payer, address _payee, uint256 _amount) payable {
-        payer = _payer;
-        payee = _payee;
-        amount = _amount;
-        released = false;
-    }
+
+constructor(address _payer, address _payee, uint256 _amount) payable {
+  tokenType = TokenType.NATIVE;
+  payer = _payer;
+  payee = _payee;
+  amount = _amount;
+  released = false;
+}
+
+constructor(address _payer, address _payee, address _tokenAddress, uint256 _amount, uint256 _tokenId, TokenType _tokenType) {
+  tokenType = _tokenType;
+  tokenAddress = _tokenAddress;
+  payer = _payer;
+  payee = _payee;
+  amount = _amount;
+  tokenId = _tokenId;
+  released = false;
+}
 
     function release() external {
         require(msg.sender == payer, "only payer can release");
--- src/Generator/templates/ProcessExecution.sol
+++ src/Generator/templates/ProcessExecution.sol
@@ -9,21 +9,45 @@ contract {{{modelID}}} {
 
   EscrowSettlement public escrow;
 
-  function createEscrow(address payee) public payable {
-    require(msg.value > 0, "amount required");
-    escrow = new EscrowSettlement{ value: msg.value }(msg.sender, payee, msg.value);
-    emit EscrowCreated(address(escrow));
-  }
+ function createEscrow(address payee) public payable {
+  require(msg.value > 0, "amount required");
+  escrow = new EscrowSettlement{ value: msg.value }(msg.sender, payee, msg.value);
+  emit EscrowCreated(address(escrow));
+}
 
-  // Rendering of Payment task
-  {{#paymentTasks}}
-  if (id == {{{taskId}}}) {
-    // payer must call enact (or enforce)
-    require(msg.sender == participants[{{{payerIndex}}}], "only payer");
-    // amount must be in wei; use encoding to provide numeric literal or variable
-    createEscrow{ value: {{{amount}}} }(participants[{{{payeeIndex}}}]);
-  }
-  {{/paymentTasks}}
+function createERC20Escrow(address payee, address tokenAddress, uint256 amount) public {
+  require(amount > 0, "amount required");
+  escrow = new EscrowSettlement(msg.sender, payee, tokenAddress, amount, 0, EscrowSettlement.TokenType.ERC20);
+  emit EscrowCreated(address(escrow));
+}
+
+function createERC721Escrow(address payee, address tokenAddress, uint256 tokenId) public {
+  escrow = new EscrowSettlement(msg.sender, payee, tokenAddress, 0, tokenId, EscrowSettlement.TokenType.ERC721);
+  emit EscrowCreated(address(escrow));
+}
+
+ // Rendering of Payment task
+{{#paymentTasks}}
+if (id == {{{taskId}}}) {
+  // payer must call enact (or enforce)
+  require(msg.sender == participants[{{{payerIndex}}}], "only payer");
+  
+  {{#isNative}}
+  // Native ETH payment
+  createEscrow{ value: {{{amount}}} }(participants[{{{payeeIndex}}}]);
+  {{/isNative}}
+  
+  {{#isERC20}}
+  // ERC-20 token payment
+  createERC20Escrow(participants[{{{payeeIndex}}}], {{{tokenAddress}}}, {{{amount}}});
+  {{/isERC20}}
+  
+  {{#isERC721}}
+  // ERC-721 NFT payment
+  createERC721Escrow(participants[{{{payeeIndex}}}], {{{tokenAddress}}}, {{{tokenId}}});
+  {{/isERC721}}
+}
+{{/paymentTasks}}
 
   {{^hasSubProcesses}}
   uint public tokenState = 1;
